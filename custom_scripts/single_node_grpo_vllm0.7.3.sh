325be19eda69de2f9aaa2168ebcc893ea9fddba6
Wenxun Wu <wenxunwu95@gmail.com>
2025-03-26 12:56:50 +0800

    Remove unnecessary localhost check for MASTER_ADDR in multi-node GRPO script

---
+++ custom_scripts/single_node_grpo_vllm0.7.3.sh
@@ -0,0 +1,43 @@
+set -x
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export NNODES=1
+export NPROC_PER_NODE=7
+export VLLM_ALLOW_LONG_MAX_MODEL_LEN=1
+
+swift rlhf \
+    --rlhf_type grpo \
+    --model ${MODEL_DIR} \
+    --reward_funcs accuracy format \
+    --use_vllm true \
+    --vllm_device auto \
+    --vllm_gpu_memory_utilization 0.5 \
+    --vllm_max_model_len ${MAX_LENGTH:-16384} \
+    --num_infer_workers 1 \
+    --train_type full \
+    --torch_dtype bfloat16 \
+    --dataset 'AI-MO/NuminaMath-TIR#10000' \
+    --max_completion_length ${MCL:-8192} \
+    --num_train_epochs 50 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --learning_rate 1e-6 \
+    --gradient_accumulation_steps 7 \
+    --eval_steps 200 \
+    --save_steps 200 \
+    --save_total_limit 2 \
+    --logging_steps 5 \
+    --max_length ${MAX_LENGTH:-16384} \
+    --output_dir ${OUTPUT_DIR} \
+    --warmup_ratio 0.01 \
+    --dataloader_num_workers 4 \
+    --dataset_num_proc 4 \
+    --num_generations 7 \
+    --temperature 0.9 \
+    --system 'examples/train/grpo/prompt.txt' \
+    --deepspeed zero2 \
+    --log_completions true \
+    --report_to swanlab \
+    --swanlab_token ${SWANLAB_TOKEN} \
+    --swanlab_project ${SWANLAB_PROJECT:-easy_r1} \
+    --swanlab_exp_name ${SWANLAB_EXP_NAME} \
+    --swanlab_mode ${SWANLAB_MODE:-cloud}
